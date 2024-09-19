319651120c6f32bec031ccf7fc83bfd5edf11804
Erika <Erika@LAPTOP-2EQ5SS9S>
2024-09-19 11:19:41 -0400

    Add project files.

---
+++ CSC222project1.cpp
@@ -0,0 +1,14 @@
+// CSC222project1.cpp : This is the starting point for two different programming projects." 
+// 
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    std::cout << "CSC222 Project1\n";
+}
