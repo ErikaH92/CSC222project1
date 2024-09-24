5f462925fdb1345cd836b37c67b37c522d6f5a02
Erika <Erika@LAPTOP-2EQ5SS9S>
2024-09-24 15:08:02 -0400

    This is one of the percentage ex branches. This program calculates and displays the discounted value of an item.
    Add project files.

--- CSC222project1.cpp
+++ CSC222project1.cpp
@@ -6,7 +6,27 @@
 
 using namespace std;
 
+// Percentage Discount
+//
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double regularPrice, percentDiscount, discountAmount, reducedPrice;
+
+	regularPrice = 59.95;
+	percentDiscount = 0.2;
+
+	discountAmount = regularPrice * percentDiscount;
+	reducedPrice = regularPrice - discountAmount;
+
+	cout << "The discount amount is $" << discountAmount << endl;
+	cout << "The discounted price is $" << reducedPrice;
+
+	return 0;
+}
 
 int main()
 {
